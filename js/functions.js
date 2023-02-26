f3e78b30dc0e70ece772d638c4e4caa25480d9f2
Алена <arslanbekova@gmail.com>
2023-02-26 23:56:32 +0300

    добавляет функцию#3

--- js/functions.js
+++ js/functions.js
@@ -47,3 +47,21 @@ const isPalindrom = (string) => {
   console.log(reverseString);
   return tempString === reverseString;
   };
+
+// Функция #3: принимает строку, извлекает цифры от 0 до 9 и возвращает их в виде целого положительного числа
+// имяФункции('2023 год');            // 2023
+// имяФункции('ECMAScript 2022');     // 2022
+// имяФункции('1 кефир, 0.5 батона'); // 105
+// имяФункции('агент 007');           // 7
+// имяФункции('а я томат');           // NaN
+
+const extractNumber = (string) => {
+  let result = '';
+  for (let i = 0; i < string.length; i++) {
+    // шаг 1. преобразовываем символы к числу шаг2.проверяем, если это не NaN - записываем в 'result'
+    if (!Number.isNaN(parseInt(string.at(i),10))) {
+    result += string.at(i);
+  }
+}
+return parseInt(result,10);
+}

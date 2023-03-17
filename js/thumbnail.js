aa1827c4025f716429ae7191ba51f1e537a23443
Алена <arslanbekova@gmail.com>
2023-03-17 23:49:31 +0300

    начала функции

---
+++ js/thumbnail.js
@@ -0,0 +1,21 @@
+const thumbnailTemplate = document
+  .querySelector('#picture');
+  .content.querySelector('.picture');
+
+const container = document.querySelector('.pictures');
+
+const createThumbnail = ( {comments, description, likes, url }) => {
+  const thumbnail = thumbnailTemplate.cloneNode(true);
+
+
+const renderThumbnails = (pictures) => {
+  const fragment = document.createDocumentFragment();
+  pictures.forEach((picture) => {
+    const thumbnail = createThumbnail(picture);
+    fragment.append(thumbnail);
+  });
+
+  container.append(fragment);
+
+  export { renderThumbnails };
+}

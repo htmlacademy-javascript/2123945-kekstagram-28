f1a9e6bbc859b9954bfdd770c96de36689c94bfd
Алена <arslanbekova@gmail.com>
2023-04-01 22:54:48 +0300

    создает getData, sendData

--- js/api.js
+++ js/api.js
@@ -12,11 +12,21 @@ const ErrorText = {
   SEND_DATA: 'Не удалось отправить форму. Попробуйте еще раз',
 };
 
-const load = (route, errorText, message = Method.GET, body = null) =>
+const load = (route, errorText, method = Method.GET, body = null) =>
   fetch(`{BASE_URL}${route}`, { method, body})
     .then((response) => {
       if(!response.ok) {
         throw new Error();
       }
       return response.json();
+    })
+    .catch(() => {
+      throw new Error(errorText);
     });
+
+const getData = () => load(Route.GET_DATA, ErrorText.GET_DATA);
+
+const sendData = (body) =>
+  load(Route.SEND_DATA, ErrorText.SEND_DATA, Method.POST, body);
+
+export { getData, sendData };

831519ae3887d04c3e829a0168e11828c1c21378
Алена <arslanbekova@gmail.com>
2023-04-06 15:02:24 +0300

    создает filter.js

---
+++ js/filter.js
@@ -0,0 +1,27 @@
+const PICTURES_COUNT = 10;
+const Filter = {
+  DEFAULT: 'filter-default',
+  RANDOM: 'filter-random',
+  DISCUSSED: 'filter-discussed',
+};
+
+const filterElement = document.querySelector('.img-filters');
+let currentFilter = Filter.DEFAULT;
+let pictures = [];
+
+const sortRandomly = () => Math.random() - 0.5;
+
+const sortByComments = (pictureA, pictureB) => {
+  pictureB.comments.length - pictureA.comments.length;
+};
+
+const getFilteredPictures = () => {
+  switch (currentFilter) {
+    case Filter.RANDOM:
+      return [...pictures].sort(sortRandomly).slice(0, PICTURES_COUNT);
+    case Filter.DISCUSSED:
+      return [...pictures].sort(sortByComments);
+    default:
+      return [...pictures];
+  }
+};
